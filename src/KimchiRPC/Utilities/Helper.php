af79bc60adc51af090cbd666c0c7136492a17f41
Zi Xing <zixing.narrakas1996@gmail.com>
2021-07-25 13:46:30 -0400

    Added method getClientIP() in \KimchiRPC\Utilities > Helper

--- src/KimchiRPC/Utilities/Helper.php
+++ src/KimchiRPC/Utilities/Helper.php
@@ -120,4 +120,79 @@
             Helper::setServerHeaders(ProtocolType::PlainText);
             print($message);
         }
+
+        /**
+         * Attempts to get the Client's IP Address
+         *
+         * @return string
+         */
+        public static function getClientIP(): string
+        {
+            if(isset($_SERVER['HTTP_CF_CONNECTING_IP']))
+            {
+                return $_SERVER['HTTP_CF_CONNECTING_IP'];
+            }
+
+            if(isset($_SERVER['HTTP_CLIENT_IP']))
+            {
+                return $_SERVER['HTTP_CLIENT_IP'];
+            }
+
+            if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+
+            if(isset($_SERVER['HTTP_X_FORWARDED']))
+            {
+                return $_SERVER['HTTP_X_FORWARDED'];
+            }
+
+            if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            {
+                return $_SERVER['HTTP_FORWARDED_FOR'];
+            }
+
+            if(isset($_SERVER['HTTP_FORWARDED']))
+            {
+                return $_SERVER['HTTP_FORWARDED'];
+            }
+
+            if(isset($_SERVER['REMOTE_ADDR']))
+            {
+                return $_SERVER['REMOTE_ADDR'];
+            }
+
+            if(getenv('HTTP_CLIENT_IP') !== False)
+            {
+                return getenv('HTTP_CLIENT_IP');
+            }
+
+            if(getenv('HTTP_X_FORWARDED_FOR'))
+            {
+                return getenv('HTTP_X_FORWARDED_FOR');
+            }
+
+            if(getenv('HTTP_X_FORWARDED'))
+            {
+                return getenv('HTTP_X_FORWARDED');
+            }
+
+            if(getenv('HTTP_FORWARDED_FOR'))
+            {
+                return getenv('HTTP_FORWARDED_FOR');
+            }
+
+            if(getenv('HTTP_FORWARDED'))
+            {
+                return getenv('HTTP_FORWARDED');
+            }
+
+            if(getenv('REMOTE_ADDR'))
+            {
+                return getenv('REMOTE_ADDR');
+            }
+
+            return '127.0.0.1';
+        }
     }
